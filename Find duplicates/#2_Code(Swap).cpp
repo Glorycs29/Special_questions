e530c329ff770c679675ea7227a1ee705a3ee876
Glory Gupta <prikarshglory@gmail.com>
2023-08-11 19:12:19 +0530

    Create #2_Code(Swap).cpp

---
+++ Find duplicates/#2_Code(Swap).cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while (nums[0] != nums[nums[0]])
+            swap(nums[0], nums[nums[0]]);
+        return nums[0];
+    }
+    // https://leetcode.com/problems/find-the-duplicate-number/editorial/
+};
