9a09f5b7ebd8c53c40d810aab871352bd408539e
Glory Gupta <prikarshglory@gmail.com>
2023-09-09 15:29:14 +0530

    Create Iterative.cpp

---
+++ N-ary Tree/Preorder traversal/Iterative.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        if (root == nullptr) {
+            return result;
+        }
+        
+        stack<Node*> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            Node* cur = stk.top();
+            stk.pop();
+            result.push_back(cur -> val);
+            for (int i = cur -> children.size() - 1; i >= 0; i--) {
+                if (cur -> children[i] != nullptr) {
+                    stk.push(cur -> children[i]);
+                }
+            }
+        }
+        return result;
+    }
+};
