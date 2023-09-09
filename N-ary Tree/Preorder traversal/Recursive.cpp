2ccceaf4f6453f10473fe1bd3c542e13a38cb510
Glory Gupta <prikarshglory@gmail.com>
2023-09-09 15:28:33 +0530

    Create Recursive.cpp

---
+++ N-ary Tree/Preorder traversal/Recursive.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+
+    void preorderHelper(Node* root, vector<int>& res){
+        if(!root) return;
+
+        res.push_back(root->val);
+        for(Node* child: root->children){
+            preorderHelper(child, res);
+        }
+    }
+
+    vector<int> preorder(Node* root) {
+        vector<int> res;
+        preorderHelper(root, res);
+        return res;
+    }
+};
